9e1a49be9ab7b59a1fac6fde60e9bc00370095c1
Bao <tranthanhbao26112004@gmail.com>
2026-01-07 14:24:11 +0700

    day la trang LibraryManager.java

---
+++ LibraryManager.java
@@ -0,0 +1,7 @@
+public class LibraryManager {
+    private List<Book> books;
+    private List<User> users;
+    private List<BorrowRecord> borrowRecords;
+    private int nextBookId;
+    private int nextUserId;
+}
